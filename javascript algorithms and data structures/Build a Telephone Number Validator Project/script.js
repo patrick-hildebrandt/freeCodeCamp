ebe9da232feb33ef3037261ba099ce12cae35705
antischock <102297813+patrick-hildebrandt@users.noreply.github.com>
2024-08-07 22:13:10 +0200

    update

--- javascript algorithms and data structures/Build a Telephone Number Validator Project/script.js
+++ javascript algorithms and data structures/Build a Telephone Number Validator Project/script.js
@@ -2,6 +2,14 @@ document.getElementById('check-btn').addEventListener('click', (e) => {
     e.preventDefault();
     const userInput = document.getElementById('user-input').value.trim();
     const resultsDiv = document.getElementById('results-div');
+    // ^                 = Anfang des Strings
+    // (1\s?)?           = Optionale Länderkennzahl "1" mit optionalem Leerzeichen
+    // (\(\d{3}\)|\d{3}) = Drei Ziffern, entweder in Klammern oder ohne Klammern
+    // ([\s-]?)          = Optionales Leerzeichen oder Bindestrich
+    // \d{3}             = Drei Ziffern (erste Teil der Nummer)
+    // ([\s-]?)          = Optionales Leerzeichen oder Bindestrich
+    // \d{4}             = Vier Ziffern (zweiter Teil der Nummer)
+    // $                 = Ende des Strings
     const validUSPhoneNumber = /^(1\s?)?(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/;
 
     if (!userInput) {
