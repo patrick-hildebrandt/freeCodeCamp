4993bbf55d7fddb6cc8395df5299d681176dc7bb
antischock <102297813+patrick-hildebrandt@users.noreply.github.com>
2024-09-16 20:10:23 +0200

    update

--- foundational c# with microsoft/TestProject/Program.cs
+++ foundational c# with microsoft/TestProject/Program.cs
@@ -194,44 +194,211 @@
 // Console.WriteLine(newMessage);
 // Console.WriteLine($"'o' appears {letterCount} times.");
 
-Console.WriteLine("a" == "a");
-Console.WriteLine("a" == "A");
-Console.WriteLine(1 == 2);
-string myValue = "a";
-Console.WriteLine(myValue == "a");
+// Console.WriteLine("a" == "a");
+// Console.WriteLine("a" == "A");
+// Console.WriteLine(1 == 2);
+// string myValue = "a";
+// Console.WriteLine(myValue == "a");
+// string value1 = " a";
+// string value2 = "A ";
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+// Console.WriteLine("a" != "a");
+// Console.WriteLine("a" != "A");
+// Console.WriteLine(1 != 2);
+// string myValue1 = "a";
+// Console.WriteLine(myValue1 != "a");
+// Console.WriteLine(1 > 2);
+// Console.WriteLine(1 < 2);
+// Console.WriteLine(1 >= 1);
+// Console.WriteLine(1 <= 1);
+// // StartsWith()
+// // EndsWith()
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
+// Console.WriteLine(pangram.Contains("fox") == false);
+// Console.WriteLine(!pangram.Contains("fox"));
+// Console.WriteLine(!pangram.Contains("fox"));
+// Console.WriteLine(!pangram.Contains("cow"));
+// int a = 7;
+// int b = 6;
+// Console.WriteLine(a != b);
+// string s1 = "Hello";
+// string s2 = "Hello";
+// Console.WriteLine(s1 != s2);
+// int saleAmount = 1001;
+// // int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+// Random random = new();
+// Console.WriteLine(random.Next(0, 2) == 0 ? "heads" : "tails");
+// string permission = "Admin|Manager";
+// int level = 55;
+// if (permission.Contains("Admin") && level > 55)
+// {
+//     Console.WriteLine("Welcome, Super Admin user.");
+// }
+// else if (permission.Contains("Admin"))
+// {
+//     Console.WriteLine("Welcome, Admin user.");
+// }
+// else if (permission.Contains("Manager") && level >= 20)
+// {
+//     Console.WriteLine("Contact an Admin for access.");
+// }
+// else if (permission.Contains("Manager"))
+// {
+//     Console.WriteLine("You do not have sufficient privileges.");
+// }
+// else
+// {
+//     Console.WriteLine("You do not have usfficient privileges.");
+// }
 
-string value1 = " a";
-string value2 = "A ";
-Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+// bool flag = true;
+// int value = 0;
+// if (flag)
+// {
+//     Console.WriteLine($"Inside the code block: {value}");
+// }
+// value = 10;
+// Console.WriteLine($"Outside the code block: {value}");
+// // if (flag)
+// //     Console.WriteLine(flag);
+// if (flag) Console.WriteLine(flag);
+// // string name = "steve";
+// // if (name == "bob") Console.WriteLine("Found Bob");
+// // else if (name == "steve") Console.WriteLine("Found Steve");
+// // else Console.WriteLine("Found Chuck");
+// string name = "steve";
+// if (name == "bob")
+//     Console.WriteLine("Found Bob");
+// else if (name == "steve")
+//     Console.WriteLine("Found Steve");
+// else
+//     Console.WriteLine("Found Chuck");
 
-Console.WriteLine("a" != "a");
-Console.WriteLine("a" != "A");
-Console.WriteLine(1 != 2);
+// int[] numbers = { 4, 8, 15, 16, 23, 42 };
+// int total = 0;
+// bool found = false;
+//
+// foreach (int number in numbers)
+// {
+//     total += number;
+//     if (number == 42)
+//         found = true;
+// }
+//
+// if (found)
+//     Console.WriteLine("Set contains 42");
+//
+// Console.WriteLine($"Total: {total}");
 
-string myValue1 = "a";
-Console.WriteLine(myValue1 != "a");
+// int employeeLevel = 100;
+// string employeeName = "John Smith";
+// string title = "";
+// switch (employeeLevel)
+// {
+//     case 100:
+//     case 200:
+//         title = "Senior Associate";
+//         break;
+//     case 300:
+//         title = "Manager";
+//         break;
+//     case 400:
+//         title = "Senior Manager";
+//         break;
+//     default:
+//         title = "Associate";
+//         break;
+// }
+// Console.WriteLine($"{employeeName}, {title}");
 
-Console.WriteLine(1 > 2);
-Console.WriteLine(1 < 2);
-Console.WriteLine(1 >= 1);
-Console.WriteLine(1 <= 1);
+// // SKU = Stock Keeping Unit. 
+// // SKU value format: <product #>-<2-letter color code>-<size code>
+// string sku = "01-MN-L";
+// string[] product = sku.Split('-');
+// string type = "";
+// string color = "";
+// string size = "";
+// switch (product[0])
+// {
+//     case "01":
+//         type = "Sweat shirt";
+//         break;
+//     case "02":
+//         type = "T-Shirt";
+//         break;
+//     case "03":
+//         type = "Sweat pants";
+//         break;
+//     default:
+//         type = "Other";
+//         break;
+// }
+// switch (product[1]) {
+//     case "BL":
+//         color = "Black";
+//         break;
+//     case "MN":
+//         color = "Maroon";
+//         break;
+//     default:
+//         color = "White";
+//         break;
+// }
+// switch (product[2]) {
+//     case "S":
+//         size = "Small";
+//         break;
+//     case "M":
+//         size = "Medium";
+//         break;
+//     case "L":
+//         size = "Large";
+//         break;
+//     default:
+//         size = "One Size Fits All";
+//         break;
+// }
+// Console.WriteLine($"Product: {size} {color} {type}");
 
-// StartsWith()
-// EndsWith()
+// // Initialisierer, Bedingung, Iterator, Hauptteil
+// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+// for (int i = 10; i > 0; i--)
+// {
+//     Console.WriteLine(i);
+// }
+// for (int i = 0; i < 10; i += 3) {
+//     Console.WriteLine(i);
+// }
+// for (int i = 0; i < 10; i++) {
+//     Console.WriteLine(i);
+//     if (i == 7) break;
+// }
+// string[] names = { "Alex", "Eddie", "David", "Michael" };
+// for (int i = names.Length - 1; i >= 0; i--) {
+//     Console.WriteLine(names[i]);
+// }
+// for (int i = 0; i < names.Length; i++) {
+//     Console.WriteLine(names[i]);
+// }
+// foreach (var name in names) {
+//     // Foreach-Iterationsvariable ist schreibgeschützt
+//     // if (name == "David") name = "Sammy";
+// }
+// for (int i = 0; i < names.Length; i++) {
+//     if (names[i] == "David") names[i] = "Sammy";
+// }
+// foreach (var name in names) Console.WriteLine(name);
 
-string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(pangram.Contains("fox"));
-Console.WriteLine(pangram.Contains("cow"));
-
-Console.WriteLine(pangram.Contains("fox") == false);
-Console.WriteLine(!pangram.Contains("fox"));
-
-Console.WriteLine(!pangram.Contains("fox"));
-Console.WriteLine(!pangram.Contains("cow"));
-
-int a = 7;
-int b = 6;
-Console.WriteLine(a != b);
-string s1 = "Hello";
-string s2 = "Hello";
-Console.WriteLine(s1 != s2);
+// for (int i = 1; i <= 100; i++) {
+//     string line = i.ToString();
+//     if (i % 3 == 0 || i % 5 == 0) line += " - ";
+//     if (i % 3 == 0) line += "Fizz";
+//     if (i % 5 == 0) line += "Buzz";
+//     Console.WriteLine(line);
+// }
