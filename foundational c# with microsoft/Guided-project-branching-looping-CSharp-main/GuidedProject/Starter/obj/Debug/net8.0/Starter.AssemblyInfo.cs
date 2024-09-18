4a42c6a143013348481c4a3af3cd7acc5960ab34
antischock <102297813+patrick-hildebrandt@users.noreply.github.com>
2024-09-18 22:17:56 +0200

    update

--- foundational c# with microsoft/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ foundational c# with microsoft/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c278ec6521d30953e5d6e1cee97a48ffa6f200")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc1c9bd8255d4215aa0d74c2d722c009c57d3d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
