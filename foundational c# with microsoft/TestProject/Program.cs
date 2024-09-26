4b5b763e1fe6631366099bd986b3ea11f7a264f5
antischock <102297813+patrick-hildebrandt@users.noreply.github.com>
2024-09-26 21:46:02 +0200

    update

--- foundational c# with microsoft/TestProject/Program.cs
+++ foundational c# with microsoft/TestProject/Program.cs
@@ -540,48 +540,116 @@
 //         Console.WriteLine(sentence.TrimStart());
 // }
 
-Console.WriteLine("Signed integral types:");
+// Console.WriteLine("Signed integral types:");
+// Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+// Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+// Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+// Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+// Console.WriteLine();
+// Console.WriteLine("Unsigned integral types:");
+// Console.WriteLine($"byte : {byte.MinValue} to {byte.MaxValue}");
+// Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+// Console.WriteLine($"uint : {uint.MinValue} to {uint.MaxValue}");
+// Console.WriteLine($"ulong : {ulong.MinValue} to {ulong.MaxValue}");
+// Console.WriteLine();
+// Console.WriteLine("Floating point types:");
+// Console.WriteLine($"float : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+// Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+// Console.WriteLine($"decimal : {decimal.MinValue} to {decimal.MaxValue} (with ~28-29 digits of precision)");
+// // int[] data;
+// // data = new int[3];
+// int[] data = new int[3];
+// Console.WriteLine("\nStringtest");
+// // string = verweistyp / implementiert als wertttyp
+// string string1 = "string";
+// string string2 = string1;
+// Console.WriteLine(string1);
+// Console.WriteLine(string2);
+// string2 += 2;
+// Console.WriteLine(string1);
+// Console.WriteLine(string2);
+// Console.WriteLine("\nDecimaltest");
+// decimal test1 = 1m;
+// int test2 = 2;
+// var test3 = test1 + test2;
+// Console.WriteLine(test3);
+// Console.WriteLine(test3.GetType().Name);
+// decimal test4 = 4.44m;
+// test3 += test4;
+// Console.WriteLine(test3);
+// Console.WriteLine(test3.GetType().Name);
 
-Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+// int first = 2;
+// string second = "4";
+// // int result = first + second;
+// string result = first + second;
+// Console.WriteLine(result);
+// // int myInt = 3;
+// // Console.WriteLine($"int: {myInt}");
+// // decimal myDecimal = myInt;
+// // Console.WriteLine($"decimal: {myDecimal}");
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
+// Console.WriteLine($"Decimal : {myDecimal}");
+// Console.WriteLine($"Float   : {myFloat}");
+// int first = 5;
+// int second = 7;
+// string message = first.ToString() + second.ToString();
+// Console.WriteLine(message);
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+// using System.Globalization;
+// CultureInfo.CurrentCulture = new CultureInfo("en-US");
+// string value1 = "5";
+// string value2 = "7";
+// // Convert rundet arithmetisch/kaufmännisch
+// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+// Console.WriteLine(result);
+// int value = (int)1.5m; // casting truncates
+// Console.WriteLine(value);
+// int value2 = Convert.ToInt32(1.5m); // converting rounds up
+// Console.WriteLine(value2);
 
-Console.WriteLine();
-Console.WriteLine("Unsigned integral types:");
+// // string value = "102";
+// string value = "bad";
+// int result = 0;
+// if (int.TryParse(value, out result))
+// {
+//     Console.WriteLine($"Measurement: {result}");
+// }
+// else
+// {
+//     Console.WriteLine("Unable to report the measurement.");
+// }
+// if (result > 0)
+//     Console.WriteLine($"Measurement (w/ offset): {50 + result}");
 
-Console.WriteLine($"byte : {byte.MinValue} to {byte.MaxValue}");
-Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-Console.WriteLine($"uint : {uint.MinValue} to {uint.MaxValue}");
-Console.WriteLine($"ulong : {ulong.MinValue} to {ulong.MaxValue}");
+// string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+// string message = "";
+// decimal total = 0.00m;
+// foreach (var value in values)
+// {
+//     decimal result;
+//     if (decimal.TryParse(value, out result))
+//         total += result;
+//     else
+//         message += value;
+// }
+// Console.WriteLine($"Message: {message}");
+// Console.WriteLine($"Total: {total}");
 
-Console.WriteLine();
-Console.WriteLine("Floating point types:");
-Console.WriteLine($"float : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal : {decimal.MinValue} to {decimal.MaxValue} (with ~28-29 digits of precision)");
-
-// int[] data;
-// data = new int[3];
-int[] data = new int[3];
-
-Console.WriteLine("\nStringtest");
-// string = verweistyp / implementiert als wertttyp
-string string1 = "string";
-string string2 = string1;
-Console.WriteLine(string1);
-Console.WriteLine(string2);
-string2 += 2;
-Console.WriteLine(string1);
-Console.WriteLine(string2);
-
-Console.WriteLine("\nDecimaltest");
-decimal test1 = 1m;
-int test2 = 2;
-var test3 = test1 + test2;
-Console.WriteLine(test3);
-Console.WriteLine(test3.GetType().Name);
-decimal test4 = 4.44m;
-test3 += test4;
-Console.WriteLine(test3);
-Console.WriteLine(test3.GetType().Name);
+// int value1 = 11;
+// decimal value2 = 6.2m;
+// float value3 = 4.3f;
+// int result1 = Convert.ToInt32(value1 / value2);
+// Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+// decimal result2 = value2 / (decimal)value3;
+// Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+// float result3 = value3 / value1;
+// Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
--- foundational c# with microsoft/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ foundational c# with microsoft/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb437a8c728065cc3e2cf2d015030b1bcf074b17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b51e9b63270fa8f92349fc187f8597def3d35ec0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
