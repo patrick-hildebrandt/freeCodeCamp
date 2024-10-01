225da8885aa6596dcd2072fc7a1cd00684f135a6
antischock <102297813+patrick-hildebrandt@users.noreply.github.com>
2024-10-01 21:10:08 +0200

    update

--- foundational c# with microsoft/TestProject/Program.cs
+++ foundational c# with microsoft/TestProject/Program.cs
@@ -653,3 +653,267 @@
 // Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 // float result3 = value3 / value1;
 // Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+// Console.WriteLine();
+// Console.WriteLine("sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+// Console.WriteLine();
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+// Console.WriteLine();
+// Console.WriteLine($"Before: {pallets[0].ToLower()}");
+// Array.Clear(pallets, 0, 2);
+// if (pallets[0] != null)
+//     Console.WriteLine($"After: {pallets[0].ToLower()}");
+// Console.WriteLine();
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     if (pallet == null)
+//         // Console.WriteLine($"-- null");
+//         Console.WriteLine($"-- {pallet}");
+//     else
+//         Console.WriteLine($"-- {pallet}");
+// }
+// Console.WriteLine();
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+// Console.WriteLine();
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+// Console.WriteLine();
+// int notNull = 0;
+// foreach (var pallet in pallets)
+//     if (pallet != null)
+//         notNull++;
+// string[] newPallets = new string[notNull];
+// Console.WriteLine($"Removing nulls ... count: {newPallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     if (pallet == null)
+//         continue;
+//     newPallets[newPallets.Length - notNull--] = pallet;
+// }
+// foreach (var pallet in newPallets)
+//     Console.WriteLine($"-- {pallet}");
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// // string result = new string(valueArray);
+// string result = String.Join(",", valueArray);
+// Console.WriteLine(result);
+// string[] items = result.Split(',');
+// foreach (string item in items)
+//     Console.WriteLine(item);
+
+// string pangram = "The quick brown fox jumps over the lazy dog";
+// string[] words = pangram.Split(' ');
+// string reversedPangram = "";
+// foreach (string word in words)
+// {
+//     char[] letters = word.ToCharArray();
+//     Array.Reverse(letters);
+//     reversedPangram += new string(letters) + " ";
+// }
+// Console.WriteLine(reversedPangram.Trim());
+
+// string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+// string[] orders = orderStream.Split(',');
+// Array.Sort(orders);
+// for (int i = 0; i < orders.Length; i++)
+// {
+//     if (orders[i].Length != 4)
+//         orders[i] += "\t- Error";
+// }
+// foreach (string order in orders)
+//     Console.WriteLine(order);
+
+// string first = "Hello";
+// string second = "World";
+// string result = string.Format("{0} {1}!", first, second);
+// Console.WriteLine(result);
+// Console.WriteLine("{1} {0}!", first, second);
+// Console.WriteLine("{0} {0} {0}!", first, second);
+// Console.WriteLine($"{first} {second}!");
+// Console.WriteLine($"{second} {first}!");
+// Console.WriteLine($"{first} {first} {first}!");
+// decimal price = 123.45m;
+// int discount = 50;
+// Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+// decimal measurement = 123456.78912m;
+// Console.WriteLine($"Measurement: {measurement:N4} units");
+// decimal tax = .36785m;
+// Console.WriteLine($"Tax rate: {tax:P2}");
+// decimal price = 67.55m;
+// decimal salePrice = 59.99m;
+// string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ",
+//     (price - salePrice), price );
+// Console.WriteLine($"A discount of {((price - salePrice)/price)}!");
+// yourDiscount += $"A discount of {((price - salePrice)/price):P2}!";
+// Console.WriteLine(yourDiscount);
+
+// int invoiceNumber = 1201;
+// decimal productShares = 25.4568m;
+// decimal subtotal = 2750.00m;
+// decimal taxPercentage = .15825m;
+// decimal total = 3185.19m;
+// Console.WriteLine($"Invoice Number: {invoiceNumber}");
+// Console.WriteLine($"   Shares: {productShares:N3} Product");
+// Console.WriteLine($"     Sub Total: {subtotal:C}");
+// Console.WriteLine($"           Tax: {taxPercentage:P2}");
+// Console.WriteLine($"     Total Billed: {total:C}");
+
+// // String-Methoden
+// // PadLeft()
+// // PadRight()
+// // Trim()
+// // TrimStart()
+// // TrimEnd()
+// // GetHashcode()
+// // Contains()
+// // StartsWith()
+// // EndsWith()
+// // Substring()
+// // Replace()
+// // Insert()
+// // Remove()
+// // Split()
+// // ToCharArray()
+// char[] chars = "das ist ein Beispiel".ToArray();
+// foreach (char ch in chars)
+//     Console.WriteLine($"{ch} {((byte)ch).ToString().PadLeft(4)}");
+
+// string input = "Pad this";
+// Console.WriteLine(input.PadLeft(12));
+// Console.WriteLine(input.PadRight(12) + "|");
+// Console.WriteLine(input.PadLeft(12, '-'));
+// Console.WriteLine(input.PadRight(12, '-'));
+// string paymentId = "769C";
+// string payeeName = "Mr. Stephen Ortega";
+// string paymentAmount = "$5,000.00";
+// var formattedLine = paymentId.PadRight(6);
+// formattedLine += payeeName.PadRight(24);
+// formattedLine += paymentAmount.PadLeft(10);
+// Console.WriteLine("1234567890123456789012345678901234567890");
+// Console.WriteLine(formattedLine);
+
+// using System.Globalization;
+// CultureInfo.CurrentCulture = new CultureInfo("en-US");
+// string customerName = "Ms. Barros";
+// string currentProduct = "Magic Yield";
+// int currentShares = 2975000;
+// decimal currentReturn = 0.1275m;
+// decimal currentProfit = 55000000.0m;
+// string newProduct = "Glorious Future";
+// decimal newReturn = 0.13125m;
+// decimal newProfit = 63000000.0m;
+// Console.WriteLine($"Dear {customerName},");
+// Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new"
+//     + $" financial product that would dramatically increase your return.\n");
+// Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n");
+// Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}. Given your current"
+//     + $" volumne, your potential profit would be {newProfit:C}.\n");
+// Console.WriteLine("Here's a quick comparison:\n");
+// string comparisonMessage = "";
+// comparisonMessage += $"{currentProduct.PadRight(20)}";
+// comparisonMessage += $"{currentReturn:P2}".PadRight(9);
+// comparisonMessage += $"{currentProfit:C}";
+// comparisonMessage += "\n";
+// comparisonMessage += $"{newProduct.PadRight(20)}";
+// comparisonMessage += $"{newReturn:P2}".PadRight(9);
+// comparisonMessage += $"{newProfit:C}";
+// Console.WriteLine(comparisonMessage);
+
+// string message = "Find what is (inside the parentheses)";
+// int openingPosition = message.IndexOf('(');
+// int closingPosition = message.IndexOf(')');
+// Console.WriteLine(openingPosition);
+// Console.WriteLine(closingPosition);
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition + 1, length - 1));
+// string message = "What is the value <span>between the tags</span>?";
+// const string openSpan = "<span>";
+// const string closeSpan = "</span>";
+// int openingPosition = message.IndexOf(openSpan);
+// int closingPosition = message.IndexOf(closeSpan);
+// openingPosition += openSpan.Length;
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+// string message = "(What if) I am (only interested) int the last (set of parentheses)?";
+// int openingPosition = message.LastIndexOf('(');
+// openingPosition += 1;
+// int closingPosition = message.LastIndexOf(')');
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+// string message = "(What if) there are (more than) one (set of parentheses)?";
+// Console.WriteLine(message);
+// while (true)
+// {
+//     int openingPosition = message.IndexOf('(');
+//     if (openingPosition == -1) break;
+//     openingPosition += 1;
+//     int closingPosition = message.IndexOf(')');
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+//     message = message.Substring(closingPosition + 1);
+// }
+// string message = "Help (find) the {opening symbols}";
+// Console.WriteLine($"Searching THIS Message: {message}");
+// char[] openSymbols = { '[', '{', '(' };
+// int startPosition = 5;
+// int openingPosition = message.IndexOfAny(openSymbols);
+// Console.WriteLine($"Found WITHOUT using startPosition: {message.Substring(openingPosition)}");
+// openingPosition = message.IndexOfAny(openSymbols, startPosition);
+// Console.WriteLine($"Found WITH using startPosition {startPosition}: {message.Substring(openingPosition)}");
+
+string message = "(What if) I have [dirreferent symbols] but every {open symbol} needs a [matching closing symbol]?";
+char[] openSymbols = { '[', '{', '(' };
+int closingPosition = 0;
+
+while (true)
+{
+    int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+    if (openingPosition == -1) break;
+    string currentSymbol = message.Substring(openingPosition, 1);
+    char matchingSymbol = ' ';
+
+    switch (currentSymbol)
+    {
+        case "[":
+            matchingSymbol = ']';
+            break;
+        case "{":
+            matchingSymbol = '}';
+            break;
+        case "(":
+            matchingSymbol = ')';
+            break;
+    }
+
+    openingPosition += 1;
+    closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+}
--- foundational c# with microsoft/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ foundational c# with microsoft/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b51e9b63270fa8f92349fc187f8597def3d35ec0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b5b763e1fe6631366099bd986b3ea11f7a264f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
