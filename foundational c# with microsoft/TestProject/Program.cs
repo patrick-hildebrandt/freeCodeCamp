f625219a7bba6f72933f455cc5a4fc44ac4fe087
antischock <102297813+patrick-hildebrandt@users.noreply.github.com>
2024-10-27 01:02:58 +0200

    update

--- foundational c# with microsoft/TestProject/Program.cs
+++ foundational c# with microsoft/TestProject/Program.cs
@@ -476,3 +476,54 @@
 // int SetTarget() => random.Next(1, 6);
 // int SetRoll() => random.Next(1, 7);
 
+// using System;
+// string[] pettingZoo =
+// {
+//     "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+//     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+//     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+// };
+// PlanSchoolVisit("School A");
+// PlanSchoolVisit("School B", 3);
+// PlanSchoolVisit("School C", 2);
+// void PlanSchoolVisit(string schoolName, int groups = 6)
+// {
+//     RandomizeAnimals();
+//     string[,] group = AssignGroup(groups);
+//     Console.WriteLine(schoolName);
+//     PrintGroup(group);
+// }
+// void RandomizeAnimals()
+// {
+//     Random random = new();
+//     for (int i = 0; i < pettingZoo.Length; i++)
+//     {
+//         int r = random.Next(i, pettingZoo.Length);
+//         string temp = pettingZoo[i];
+//         pettingZoo[i] = pettingZoo[r];
+//         pettingZoo[r] = temp;
+//     }
+// }
+// string[,] AssignGroup(int groups = 6)
+// {
+//     // Gruppengröße: potentieller Fehler =>
+//     // Console.WriteLine(19 / groups);
+//     // Console.WriteLine(19 % groups);
+//     string[,] result = new string[groups, pettingZoo.Length / groups];
+//     int start = 0;
+//     for (int i = 0; i < groups; i++)
+//         for (int j = 0; j < result.GetLength(1); j++)
+//             result[i, j] = pettingZoo[start++];
+//     return result;
+// }
+// void PrintGroup(string[,] group)
+// {
+//     for (int i = 0; i < group.GetLength(0); i++)
+//     {
+//         Console.Write($"Group {i + 1}: ");
+//         for (int j = 0; j < group.GetLength(1); j++)
+//             Console.Write($"{group[i, j]}  ");
+//         Console.WriteLine();
+//     }
+// }
+
--- foundational c# with microsoft/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ foundational c# with microsoft/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b5b869b4890e87abab9b5a7b43f260018541ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+298b749f131bcd2039fab61b954c28ca95ea605b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
