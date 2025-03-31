91425e724b485a3fec6eb3c1f9b0af0d91828c28
patHildebrandt <patrick@koeper-it.de>
2025-03-31 23:19:27 +0200

    update

--- foundational c# with microsoft/Exceptions101/obj/Debug/net9.0/Exceptions101.AssemblyInfo.cs
+++ foundational c# with microsoft/Exceptions101/obj/Debug/net9.0/Exceptions101.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Exceptions101")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ed9f81ebf5309f84c5dbf11260cd273e91f5c08")]
-[assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
+[assembly: System.Reflection.AssemblyProductAttribute("Exceptions101")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Exceptions101")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
